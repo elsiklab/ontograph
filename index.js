06a1f6db95e09288e40481f23afc9b66634f7000
Colin <colin.diesh@gmail.com>
2015-08-10 18:09:19 -0500

    Add zoom widget

--- index.js
+++ index.js
@@ -6,26 +6,22 @@ var request=require('request');
 request('http://localhost/~biocmd/ontograph/gene_ontology.json', function(err,res,body) {
     var graph=JSON.parse(body);
     // Create the input graph
-    console.log(graph);
     var g = new dagreD3.graphlib.Graph()
       .setGraph({})
-      .setDefaultEdgeLabel(function() { return {}; })
-      .setDefaultNodeLabel(function() { return 'name'; });
+      .setDefaultEdgeLabel(function() { return {}; });
 
     // Here we"re setting nodeclass, which is used by our custom drawNodes function
     // below.
 
     for(var i=0; i<graph.length; i++) {
         var node=graph[i];
-        console.log(node.id);
-        g.setNode(node.id, node.name);
+        g.setNode(node.id, node);
         if(i>25) break;
     }
     
 
     for(var i=0; i<graph.length; i++) {
         var node=graph[i];
-        console.log(node.id,node.parent);
         if(node.parent) {
             g.setEdge(node.id,node.parent);
         }
@@ -37,15 +33,31 @@ request('http://localhost/~biocmd/ontograph/gene_ontology.json', function(err,re
 
     // Set up an SVG group so that we can translate the final graph.
     var svg = d3.select("svg"),
-        svgGroup = svg.append("g");
+        inner = svg.append("g");
+
+    // Set up zoom support
+    var zoom = d3.behavior.zoom().on("zoom", function() {
+      inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                  "scale(" + d3.event.scale + ")");
+    });
+    svg.call(zoom);
+
+
+    // Create the renderer
+    var render = new dagreD3.render();
 
     // Run the renderer. This is what draws the final graph.
-    render(d3.select("svg g"), g);
+    render(inner, g);
 
     // Center the graph
-    var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
-    svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
-    svg.attr("height", g.graph().height + 40);
+    var initialScale = 0.75;
+    zoom
+      .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+      .scale(initialScale)
+      .event(svg);
+    svg.attr('height', g.graph().height * initialScale + 40);
+   
+
 });
 
 
