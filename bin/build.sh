70b79def625f712591fd18eafe8d442063800f74
Colin <colin.diesh@gmail.com>
2015-08-10 18:08:41 -0500

    Automatically build GO JSON

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
+set -e
+set -u
+set -v
 rm -rf dist
 mkdir -p dist/js
 mkdir -p dist/css
 cp index.html dist/
+[ -f go.obo ] || wget http://www.geneontology.org/ontology/go.obo
+[ -f gene_ontology.json ] || perl scripts/read-obo.pl go.obo > gene_ontology.json
 browserify index.js | uglifyjs > dist/js/index.js
