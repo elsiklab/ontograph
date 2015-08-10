7ec4e3beff2775afbf37c591460aac276272ec1e
Colin <colin.diesh@gmail.com>
2015-08-10 12:51:09 -0500

    Add read-obo parser

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,4 @@ rm -rf dist
 mkdir -p dist/js
 mkdir -p dist/css
 cp index.html dist/
-cp demo.js dist/js/
 browserify index.js | uglifyjs > dist/js/index.js
---
+++ scripts/read-obo.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+use GO::Parser;
+
+
+my $parser = new GO::Parser({handler=>'obj',use_cache=>1});
+
+$parser->parse($ARGV[0]);
+
+my $graph = $parser->handler->graph;
+$graph->iterate(sub {
+	my $ni=shift;
+	if($ni->{'term'}->{'is_root'} && !$ni->{'term'}->{'is_obsolete'} && !$ni->{'term'}->{'is_relationship_type'}) {
+		print "{ id: '".$ni->{'term'}->{'acc'}."', name:'".$ni->{'term'}->{'name'}."' },\n";
+	}
+	if(!$ni->{'term'}->{'is_root'} && !$ni->{'term'}->{'is_obsolete'} && !$ni->{'term'}->{'is_relationship_type'}) {
+		print "{ id: '".$ni->{'term'}->{'acc'}."', name:'".$ni->{'term'}->{'name'}."', parent: '".$ni->{'parent_rel'}->{'acc1'}."' },\n";
+	}
+});
+
