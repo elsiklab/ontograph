a8bcdbcb6eee635093179f478a69034371af784d
Colin <colin.diesh@gmail.com>
2016-06-02 11:54:55 -0500

    Add travis-ci and tidy-html5

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ function process_parents_edges(cy, graph, term, depth) {
                     edges_cy[edge_name] = {
                         data: {
                             label: elt,
+                            id: edge_name,
                             target: target,
                             source: source,
                         },
@@ -183,7 +184,7 @@ function setup_graph( graph, term ) {
     // Manually crate and stop layout after timeout
     var layout = $('#layout option:selected').text();
     var layout_cy = cy.makeLayout({
-        name: layout,
+        name: layout.trim(),
         rankDir: 'BT',
         padding: 50,
         randomize: true,
@@ -279,6 +280,18 @@ $( function() {
         window.history.replaceState( {}, '', '?term=' + term );
         download_and_setup_graph(term);
         return false;
+    });
+
+    var terms = $('#goterms').val();
+
+    // Process textarea from form
+    var nodes=[],pvals=[];
+    terms.split('\n').forEach(function(line) {
+        var matches = line.split('\t');
+        console.log(matches[0],matches[1])
+
+        nodes.push(matches[0]);
+        pvals.push(matches[1]);
     });
 });
 
